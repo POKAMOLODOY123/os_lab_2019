4f4470c1723ab885a4ea14b9d78083025d93731d
POKAMOLODOY123 <59928269+POKAMOLODOY123@users.noreply.github.com>
2024-10-11 23:01:18 +0000

    Lab2 Complete

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int len = strlen(str);
+  char *reversed = malloc(len + 1);
+  int j = 0;
+  for (int i = len - 1; i >= 0; i--) {
+    reversed[j++] = str[i];
+  }
+  reversed[j] = '\0';
+  strcpy(str, reversed);
+  free(reversed);
 }
-
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,16 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+	char temp = *left;
+  	*left = *right;
+  	*right = temp;
+}
+
+int main() 
+{
+  char a = 'a';
+  char b = 'b';
+  Swap(&a, &b);
+  printf("%c %c\n", a, b);
+  return 0;
 }
