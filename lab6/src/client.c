e583cc7c358c4e066f2781cf590d5df3c82a253e
POKAMOLODOY123 <59928269+POKAMOLODOY123@users.noreply.github.com>
2024-11-23 07:01:01 +0000

    Lab6 complete

--- lab6/src/client.c
+++ lab6/src/client.c
@@ -16,17 +16,7 @@ struct Server {
     int port;
 };
 
-uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-    uint64_t result = 0;
-    a = a % mod;
-    while (b > 0) {
-        if (b % 2 == 1)
-            result = (result + a) % mod;
-        a = (a * 2) % mod;
-        b /= 2;
-    }
-    return result % mod;
-}
+#include "utils.h"
 
 bool ConvertStringToUI64(const char *str, uint64_t *val) {
     char *end = NULL;
--- lab6/src/server.c
+++ lab6/src/server.c
@@ -17,17 +17,7 @@ struct FactorialArgs {
     uint64_t mod;
 };
 
-uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-    uint64_t result = 0;
-    a = a % mod;
-    while (b > 0) {
-        if (b % 2 == 1)
-            result = (result + a) % mod;
-        a = (a * 2) % mod;
-        b /= 2;
-    }
-    return result % mod;
-}
+#include "utils.h"
 
 uint64_t Factorial(const struct FactorialArgs *args) {
     uint64_t ans = 1;
